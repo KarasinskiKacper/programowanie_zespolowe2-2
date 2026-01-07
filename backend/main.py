c6374a1b453682a9626095f40c7196b9a494083d
Paweł Dyczek <60507555+DyczekPawel@users.noreply.github.com>
2026-01-07 17:19:41 +0100

    Feature/basic api (#2)
    * Add database objects definition and basic main file
    
    * Fix gitignore
    
    * Add basic API routes
    
    * Add requirements.txt
    
    ---------
    
    Co-authored-by: Arisen2123 <pawelherzyk2002@gmail.com>

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from db_objects import db
+from flask_jwt_extended import JWTManager
+
+
+def create_app():
+        
+    load_dotenv()
+    
+    app = Flask(__name__)
+    CORS(app)
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    jwt = JWTManager(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
